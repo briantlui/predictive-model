c8afd31282ee6e3068fbdf9f1387e97643d02a56
briantlui <bbl566@gmail.com>
2025-04-07 23:04:08 -0400

    Added 2 feature importance plots. Adding streamlit files for demo

---
+++ src/streamlit/Hotel Cancellation Predictor.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Introduction Page
+st.title("Welcome to the Hotel Cancellation Predictor!")
+st.markdown(
+    """
+    **Problem:** Is your hotel struggling to reach a "perfect sellout"?  Are you
+    
+    **Solution:** This app predicts the likelihood of cancellations, helping you make data-driven decisions.
+    
+    **Navigate to Pages:**
+    - Use the sidebar to explore the recommender system and other features.
+    """
+)
---
+++ src/streamlit/pages/Contact.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Introduction Page
+st.title("About Me")
+st.markdown(
+    """
+    I'm Brian, I'm a revenue management hotelier turned data scientist. 
+    
+    **Contact Information**
+
+    Email: bbl566@gmail.com
+
+    Linked In: 
+    """
+)
